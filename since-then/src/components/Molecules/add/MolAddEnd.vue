60585f0c330ac980c00fac6ec7aac4f281745a2b
yuto <flyingfox4087@yahoo.co.jp>
2021-06-30 23:38:45 +0900

    確認画面を作成

--- since-then/src/components/Molecules/add/MolAddEnd.vue
+++ since-then/src/components/Molecules/add/MolAddEnd.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="mol-add-end">
-    <h2>この内容で登録します</h2>
+    <h2>この内容で登録します。よろしいですか？</h2>
+    <div>
+      <label>【日付】</label>
+      <p>{{ formatDate() }}</p>
+    </div>
+    <div>
+      <label>【タイトル】</label>
+      <p>{{ store.state.title }}</p>
+    </div>
+    <div>
+      <label>【メモ】</label>
+      <p>{{ store.state.memo }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import { key } from "@/store";
 
 export default defineComponent({
   name: "MolAddEnd",
-  components: {},
+  setup() {
+    const store = useStore(key);
+
+    const formatDate = (): string => {
+      const hyphenDate: string = store.state.date;
+      const replaceDate = hyphenDate.replace(/-/g, "/");
+      //const date = Date.parse(stringDate);
+
+      return replaceDate;
+    };
+
+    return {
+      store,
+      formatDate,
+    };
+  },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+}
+</style>
