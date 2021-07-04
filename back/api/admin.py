386334e966084df2b6d1ce55620972ae1fc2b9b9
yuto <flyingfox4087@yahoo.co.jp>
2021-07-04 23:37:47 +0900

    管理画面にmodelを登録

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import PostDate, UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    # 一覧表示画面のフィールド
+    list_display = ('name', 'email', 'is_active', 'is_staff')
+    # フィールドをリンク化
+    list_display_links = ('name', 'email')
+
+
+class PostDateAdmin(admin.ModelAdmin):
+    list_display = ('date', 'title', 'memo', 'created_datetime',
+                    'updated_datetime', 'author')
+    list_display_links = ('title', 'date', 'memo')
+
+
+# 管理画面に登録
+admin.site.register(PostDate, PostDateAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
