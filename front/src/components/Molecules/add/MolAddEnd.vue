9034fa276e0fb1c14da3f1c9eed3d5f4dd161296
yuto <solidusgako4087@gmail.com>
2021-07-07 13:42:03 +0900

    確認画面のレイアウトを修正

--- front/src/components/Molecules/add/MolAddEnd.vue
+++ front/src/components/Molecules/add/MolAddEnd.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="mol-add-end">
     <h2>この内容で登録します。よろしいですか？</h2>
-    <div>
-      <label>【日付】</label>
-      <p>{{ formatDateToString() }}</p>
-    </div>
-    <div>
-      <label>【タイトル】</label>
-      <p>{{ store.state.inputTitle }}</p>
-    </div>
-    <div>
-      <label>【メモ】</label>
-      <p>{{ store.state.inputMemo }}</p>
-    </div>
+
+    <table>
+      <!-- テーブルセル幅設定 -->
+      <colgroup>
+        <col style="width: 33%" />
+        <col style="width: 33%" />
+      </colgroup>
+
+      <tbody>
+        <tr>
+          <td>【日付】</td>
+          <td align="left">{{ formatDateToString() }}</td>
+        </tr>
+        <tr>
+          <td>【タイトル】</td>
+          <td align="left">{{ store.state.inputTitle }}</td>
+        </tr>
+        <tr>
+          <td>【メモ】</td>
+          <td align="left">{{ store.state.inputMemo }}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
@@ -52,5 +64,23 @@ export default defineComponent({
 <style lang="scss" scoped>
 ul {
   list-style: none;
+}
+
+.validate {
+  color: red;
+}
+
+table {
+  table-layout: fixed;
+  width: 30%; /* テーブルのセル幅固定 */
+  margin-left: auto;
+  margin-right: auto;
+  border: 3px solid #42b983;
+}
+
+table td {
+  /*table内のtdに対して*/
+  word-wrap: break-word; // 改行させる設定
+  padding: 10px 0px;
 }
 </style>
