9adf219f369ad1fb63843ec7f0c6a907da2fb4bc
yuto <solidusgako4087@gmail.com>
2021-07-15 00:15:00 +0900

    作成日時、更新日時を追加

--- front/src/components/Molecules/home/MolHomeDetail.vue
+++ front/src/components/Molecules/home/MolHomeDetail.vue
@@ -1,31 +1,54 @@
 <template>
   <div id="mol-home-detail">
-    {{ state.postDetail }}
+    <table>
+      <!-- テーブルセル幅設定 -->
+      <colgroup>
+        <col style="width: 33%" />
+        <col style="width: 33%" />
+      </colgroup>
 
-    <AtomInputDate :defaultDate="state.postDetail['date']" />
-    <AtomInput
-      :placeholder="'タイトル名を入力してください'"
-      :defaultTitle="state.postDetail['title']"
-    />
-    <AtomTextarea
-      :placeholder="'メモ等あればを入力してください'"
-      :defaultText="state.postDetail['memo']"
-    />
+      <tr>
+        <th>タイトル</th>
+        <td class="column">
+          <AtomInputDate :defaultDate="state.postDetail['date']" />
+        </td>
+      </tr>
+      <tr>
+        <th>日付</th>
+        <td class="column">
+          <AtomInput
+            :placeholder="'タイトル名を入力してください'"
+            :defaultTitle="state.postDetail['title']"
+          />
+        </td>
+      </tr>
+      <tr>
+        <th>メモ</th>
+        <td class="column">
+          <AtomTextarea
+            :placeholder="'メモ等あればを入力してください'"
+            :defaultText="state.postDetail['memo']"
+          />
+        </td>
+      </tr>
+      <tr>
+        <th>作成日時</th>
+        <td class="column">
+          {{ formatDateToString(state.postDetail["created_datetime"]) }}
+        </td>
+      </tr>
+      <tr>
+        <th>更新日時</th>
+        <td class="column">
+          {{ formatDateToString(state.postDetail["updated_datetime"]) }}
+        </td>
+      </tr>
+    </table>
 
-    <div class="content">
-      <div class="inner">
-        <AtomButton :text="'戻る'" @click="transitionTable" />
-        <AtomButton
-          :text="'更新'"
-          @click="transitionTable"
-          btnColor="#00CCFF"
-        />
-        <AtomButton
-          :text="'削除'"
-          @click="transitionTable"
-          btnColor="#FF4F50"
-        />
-      </div>
+    <div class="btn">
+      <AtomButton :text="'戻る'" @click="transitionTable" style="padding:0px 3%;" />
+      <AtomButton :text="'更新'" @click="transitionTable" btnColor="#00CCFF" style="padding:0px 3%;" />
+      <AtomButton :text="'削除'" @click="transitionTable" btnColor="#FF4F50" style="padding:0px 3%;" />
     </div>
   </div>
 </template>
@@ -71,10 +94,33 @@ export default defineComponent({
       context.emit("emitTable");
     };
 
+    // DateオブジェクトをYYYYMMDD hh:mmに変換
+    const formatDateToString = (strDate: string): string => {
+      // Dateオブジェクトをインスタンス化
+      const date = new Date(strDate);
+
+      const yearStr = date.getFullYear();
+      //月だけ+1すること
+      const monthStr = 1 + date.getMonth();
+      const dayStr = date.getDate();
+      const hourStr = date.getHours();
+      const minuteStr = date.getMinutes();
+
+      let formatDate = "YYYY-MM-DD hh:mm";
+      formatDate = formatDate.replace(/YYYY/g, `${yearStr}`);
+      formatDate = formatDate.replace(/MM/g, `${monthStr}`);
+      formatDate = formatDate.replace(/DD/g, `${dayStr}`);
+      formatDate = formatDate.replace(/hh/g, `${hourStr}`);
+      formatDate = formatDate.replace(/mm/g, `${minuteStr}`);
+
+      return formatDate;
+    };
+
     return {
       store,
       state,
       transitionTable,
+      formatDateToString,
     };
   },
 });
@@ -84,14 +130,26 @@ export default defineComponent({
 #mol-home-detail {
   text-align: center;
 }
-.content {
-  width: 400px;
-  height: 100px;
+
+.btn {
+  display: flex;
+  justify-content: center;
+  padding-top: 5%;
 }
 
-.inner {
-  padding-top: 30px;
-  display: flex;
-  justify-content: space-evenly;
+table {
+  table-layout: fixed;
+  width: 40%; /* テーブルのセル幅固定 */
+  margin-left: auto;
+  margin-right: auto;
+  //border: 3px solid #42b983;
+}
+tr {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.05);
+}
+
+.column {
+  word-wrap: break-word; // 改行させる設定
+  padding: 40px 0px;
 }
 </style>
