0c54d651a0ef5cc9f6eb151b7b2aa1cabd5f8edd
yuto90 <solidusgako4087@gmail.com>
2021-08-02 21:55:46 +0900

    ログイン機能を実装

--- front/src/components/Organisms/OrgLogin.vue
+++ front/src/components/Organisms/OrgLogin.vue
@@ -1,13 +1,99 @@
 <template>
-  <div id="org-login">Login</div>
+  <div id="org-login">
+    <div class="Form">
+      <div class="Form-Word">
+        <h2>ログイン</h2>
+      </div>
+
+      <MolEmailForm @emitEmail="setInputEmail" />
+
+      <MolPassForm @emitPass="setInputPass" />
+
+      <div class="form-register-button">
+        <AtomButton @click="registerUser" :text="'ログイン'" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { key } from "@/store";
+import axios from "axios";
+
+import AtomButton from "@/components/Atoms/AtomButton.vue";
+
+import MolEmailForm from "@/components/Molecules/MolEmailForm.vue";
+import MolPassForm from "@/components/Molecules/MolPassForm.vue";
 
 export default defineComponent({
-  name: "OrgAuthLogin",
+  name: "OrgLogin",
+  components: {
+    AtomButton,
+    MolEmailForm,
+    MolPassForm,
+  },
+  setup() {
+    const store = useStore(key);
+    const router = useRouter();
+
+    const state = reactive({
+      displayInputEmail: "",
+      displayInputPass: "",
+    });
+
+    const setInputEmail = (inputEmail: string) => {
+      state.displayInputEmail = inputEmail;
+    };
+
+    const setInputPass = (inputPass: string) => {
+      state.displayInputPass = inputPass;
+    };
+
+    const registerUser = async () => {
+      // ユーザーログイン
+      await axios
+        .post("http://127.0.0.1:8000/login/", {
+          email: state.displayInputEmail,
+          password: state.displayInputPass,
+        })
+        .then((response) => {
+          store.commit("setToken", "JWT " + response.data["token"]);
+        })
+        .catch((error) => console.log(error));
+
+      // Homeにリダイレクト
+      router.push("/");
+    };
+
+    return {
+      setInputEmail,
+      setInputPass,
+      registerUser,
+    };
+  },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#org-login {
+  .Form {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 720px;
+  }
+  @media screen and (max-width: 480px) {
+    .Form {
+      margin-top: 40px;
+    }
+  }
+  .Form-Word {
+    text-align: center;
+  }
+  .form-register-button {
+    text-align: center;
+  }
+}
+</style>
